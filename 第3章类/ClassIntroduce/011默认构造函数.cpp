cd781ffa3c484830f86ab3a05c63adf59f1d5693
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-03-20 15:32:34 +0800

    Update 011默认构造函数.cpp

--- 第3章类/ClassIntroduce/011默认构造函数.cpp
+++ 第3章类/ClassIntroduce/011默认构造函数.cpp
@@ -17,7 +17,7 @@ public:
 
 };
 
-Time::Time(int TemHour, int TemMinute, int TemSecond) :Hour(TemHour), Minute(TemMinute), Second(TemSecond)//����ʽ������ֵ����Ա����
+Time::Time(int TemHour, int TemMinute, int TemSecond) :Hour(TemHour), Minute(TemMinute), Second(TemSecond)//把形式参数赋值给成员属性
 {
 }
 Time::Time(int Hour)
@@ -25,12 +25,30 @@ Time::Time(int Hour)
 	this->Hour = Hour;
 }
 
+class A
+{
+public:
+	//含有一个B的成员变量，而B没有不带参数的默认构造函数，
+	//所以当A不含有构造函数时，编译器合成的A的默认构造函数里面会去尝试调用B的默认构造函数，但是B没有不带参数的默认构造函数
+	
+	A():bb(10)//这种情况下合成的默认构造函数是不够用的，我们得手工鞋一个
+	{
+	}
+	B bb;
+};
+class B
+{
+public:
+	B(int)
+	{
+	}
+}
 
 int main(void)
 {
 	//Time myTime01 = Time(12, 12, 12);
 
-	Time myTime02;	//����Ĭ�Ϲ��캯��
+	Time myTime02;	//调用默认构造函数
 	system("pause");
 	return 0;
 
@@ -38,11 +56,11 @@ int main(void)
 
 
 /*
-*(1)Ĭ�Ϲ��캯��--û���κβ����Ĺ��캯��
-*	1.���û���κι��캯����ϵͳĬ�ϸ�һ��Ĭ�Ϲ��캯�����������Զ�����һ��Ĭ�Ϲ��캯��������С��ϳɵ�Ĭ�Ϲ��캯����
-*	2.һ������д�˹��캯����ϵͳ�Ͳ����Զ�����Ĭ�Ϲ��캯����
-*(2)������������ʱ�򣬻����������Ķ��󣬱���ʹ�ó�Ա��ʼ���б��ͳ�ʼֵ���ֹ�д�Լ��Ĺ��캯����
+*(1)默认构造函数--没有任何参数的构造函数
+*	1.如果没有任何构造函数，系统默认给一个默认构造函数，编译器自动定义一个默认构造函数，这个叫“合成的默认构造函数”
+*	2.一旦我们写了构造函数，系统就不会自动创建默认构造函数。
+*(2)当进行类的组合时候，会包含其他类的对象，必须使用成员初始化列表和初始值，手工写自己的构造函数。
 *(3)
-*2019��11��17��09:11:05 Sunrise�ڶ���������ѧ�ڶ���ѧ¥1121ʵ����
+*2019年11月17日09:11:05 Sunrise于东北电力大学第二教学楼1121实验室
 *
 */
