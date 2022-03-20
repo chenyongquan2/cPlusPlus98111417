5f44d547923d1b1110060dc395853ee193dc9c7f
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-03-20 12:54:25 +0800

    Update 005在类定义中实现成员函数inline.cpp

--- 第3章类/ClassIntroduce/005在类定义中实现成员函数inline.cpp
+++ 第3章类/ClassIntroduce/005在类定义中实现成员函数inline.cpp
@@ -14,9 +14,10 @@ public:
 	explicit Time(int Hour, int Minute, int Second);
 	Time(int Hour);
 public:
-	void AddHour(int TemHour)
+	void AddHour(int TemHour)//问题：如果这个类实现的函数在.h中，若被多次include函数体，是否会像普通的成员函数那般说会被重复定义？
+	//不会，因为这种函数被当做inline内联函数处理,不会报重复定义的错。
 	{
-		/*(1)在类定义中实现成员函数inline，类内的成员函数实现其实也叫类的成员函数定义。
+		/*(1)在类定义中实现成员函数inline(宏展开，减少入栈出栈的开销)，类内的成员函数实现其实也叫类的成员函数定义。
 *	AddInline()函数在类的定义中实现的成员函数，会被当做inline内联函数来处理。
 *	能不能内联成功，取决于具体的编译器。*/
 		
