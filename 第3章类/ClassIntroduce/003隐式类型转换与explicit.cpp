c2e6ed5b8404801384c0e979499e641c4f147440
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-03-20 12:34:53 +0800

    Update 003隐式类型转换与explicit.cpp

--- 第3章类/ClassIntroduce/003隐式类型转换与explicit.cpp
+++ 第3章类/ClassIntroduce/003隐式类型转换与explicit.cpp
@@ -16,7 +16,7 @@ public:
 
 };
 
-Time::Time(int Hour, int Minute, int Second)//����ʽ������ֵ����Ա����
+Time::Time(int Hour, int Minute, int Second)//把形式参数赋值给成员属性
 {
 	this->Hour = Hour;
 	this->Minute = Minute;
@@ -34,21 +34,21 @@ void functionTransform(Time TemTime)
 
 int main(void)
 {
-	Time myTime01 = 12;
-	//Time myTime03(12, 23, 43, 54);	//����
+	Time myTime01 = 12;//Todo:编译系统有个行为，将12这个数字转化成了一个Time类型，存在了临时对象隐式转换问题。
+	//Time myTime03(12, 23, 43, 54);	//错误
 	Time myTime04 = { 16 };
-	//�������ڲ����Ե���һ������	--�����һ�������Ĺ��캯����explicit�ͻ������ʽ����ת�����������
-	Time myTime02=(12, 23, 43, 54);	//����õ������Ĺ��캯��--�൱�ڶ��ű���ʽ�������Hour=54
+	//大括号内部可以当做一个对象·	--如果带一个参数的构造函数加explicit就会进行隐式类型转换，都会错误
+	Time myTime02=(12, 23, 43, 54);	//对()内部发生了隐式类型转换，把()转化为对象， “调用了单参数的构造函数”--相当于逗号表达式，最后是Hour=54
 
-	functionTransform(12);	//���Ȼ����һ�������Ĺ��죬����һ��Time��ʱ����
+	functionTransform(12);	//首先会调用一个参数的构造，构造一个Time临时对象，存在了临时对象隐式转换问题。
 
-	//explicit��ֹ��ʽ����ת��
-	//Time myTime05 = { 12,12,12 };	//���󣬲�����ʽ����ת��
-	Time myTime06(2, 12, 34);//��ȷ�����ù��캯��
-	Time myTime07=(2, 12, 34);//��ȷ�����ù��캯��
+	//explicit防止隐式类型转化
+	//Time myTime05 = { 12,12,12 };	//错误，不能隐式类型转换
+	Time myTime06(2, 12, 34);//正确，调用构造函数
+	Time myTime07=(2, 12, 34);//正确，调用构造函数
 	/*
-	 * �ܽ᣺һ�������Ϊ()�ǲ�����{}��һ������
-	 * (1)���ڵ��������Ĺ��캯����һ������Ϊexplicit,����������ԭ��
+	 * 总结：一般可以认为()是参数，{}是一个对象
+	 * (1)对于单个参数的构造函数，一般声明为explicit,除非有特殊原因。
 	 */
 
 	system("pause");
@@ -56,10 +56,10 @@ int main(void)
 
 }
 /*
- *(1)��ʽ����ת����explicit
- *		����ϵͳ˽�����˺ܶ����ǲ�֪�������飬�����������������Ͳ�ƥ��ʱ��ͻ������ʽ����ת��
- *(2)�Ƿ����ǿ��ϵͳ��ȷҪ���캯������ǿ������ת����
- *	���ԡ�������캯��������explicit(��ʽ��)����������캯��ֻ�����ڳ�ʼ����ʽ����ת�������ܽ�����ʽ����ת����
+ *(1)隐式类型转换和explicit
+ *		编译系统私下做了很多我们不知道的事情，当参数不够或者类型不匹配时候就会进行隐式类型转换
+ *(2)是否可以强制系统明确要求构造函数不做强制类型转换？
+ *	可以。如果构造函数中声明explicit(显式的)。则这个构造函数只能用于初始化显式类型转换。不能进行隐式类型转化。
  *(3)
  * 
  */
