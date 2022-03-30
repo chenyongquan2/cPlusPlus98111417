930f69caf32c198100537d8a28d3b9b2ba47fb48
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-03-30 21:21:22 +0800

    Update 007unique_ptr使用方法总结和智能指针概述.cpp

--- 第5章内存管理/MemoryManage/007unique_ptr使用方法总结和智能指针概述.cpp
+++ 第5章内存管理/MemoryManage/007unique_ptr使用方法总结和智能指针概述.cpp
@@ -12,9 +12,10 @@ unique_ptr<string>myFunction()
 	unique_ptr<string>pr(new string("dsfsdf"));
 	return  pr;
 	/*
-	 * �Ӻ�������һ���ֲ�����ϵͳ����������һ����ʱunique_ptr���󣬵���unique_ptr���ƶ����캯��
-	 * 
+	 * 从函数返回一个局部对象，系统给我们生成一个临时unique_ptr对象，调用unique_ptr的移动构造函数
+	 * 返回这种局部对象，导致系统给我们生成一个临时的unique_ptr对象，调用unique_ptr的移动构造函数给临时对象。
 	 */
+	//return  unique_ptr<string>pr(new string("dsfsdf"));//更好
 }
 
 void myDelete(string *pdel)
@@ -23,11 +24,11 @@ void myDelete(string *pdel)
 	pdel = nullptr;
 }
 typedef void(*myf)(string *);
-//���庯��ָ������ using myf=void(*)(string*)
+//定义函数指针类型 using myf=void(*)(string*)
 //typedef decltype(myDelete)*myf;
 
-//lambda����ʽ
-auto mydel=[](string*del)
+//lambda表达式
+auto mydel=[](string*del)//把lamdba表达式理解为一个class
 {
 	delete del;
 	del = nullptr;
@@ -37,18 +38,20 @@ auto mydel=[](string*del)
 int main(void)
 {
 	unique_ptr<string> p1;
-	p1 = myFunction();//��ʱ����ֱ�ӹ�����p1�С������ܻ��ͷŵ���ʱ��������ָ����ڴ�ռ䡣
+	//虽然uniqie_ptr智能指针不能拷贝，但是，当这个unique_ptr将要被销毁，是可以拷贝给别的unique_prt接收的。最常见的就是接收函数返回的一个unique_ptr
+	p1 = myFunction();//临时对象直接构造在p1中。不接受会释放掉临时对象及其所指向的内存空间。
 
 
-	//ָ��ɾ����--ȡ��ϵͳĬ�ϵ�ɾ����
+	//指定删除器--取代系统默认的删除器
 	unique_ptr<string, myf> p2(new string("jisuanj"), myDelete);
 
-	//��һ��д��
-	unique_ptr<string, decltype(myDelete)*>p3(new string("jisann"), myDelete);
-	//��lambda����ʽ��д����������ɴ���operator()���͵������
-	unique_ptr<string, decltype(mydel)>p4(new string("sdfdsf"), mydel);
+	//另一种写法
+	unique_ptr<string, decltype(myDelete)*>p3(new string("jisann"), myDelete);//大小为8字节
+	//用lambda表达式来写，可以理解成带有operator()类型的类对象
+	unique_ptr<string, decltype(mydel)>p4(new string("sdfdsf"), mydel);//大小为4字节
+	//所以decltype(mydel)可以理解为带有operator()类类型对象
 	
-	//unque_ptr����ָ����ͬ
+	//unque_ptr和裸指针相同
 	string *p = nullptr;
 	cout << sizeof(p) << endl;//4
 	unique_ptr<string>p5(new string("sdf"));
@@ -59,42 +62,56 @@ int main(void)
 }
 
 /*
-*(1)����unique_ptr����ָ��
-*�ǲ��ܿ�����Ҫ�����ٵ�ʱ�򣬻��ǿ��Կ����ģ�������Ǵ�һ����������һ��unique_ptr
+*(1)返回unique_ptr智能指针
+*是不能拷贝，要被销毁的时候，还是可以拷贝的，最常见的是从一个函数返回一个unique_ptr
 *
-*(2)ָ��ɾ����
-*	1.ȱʡ����£�����delete�ͷ��ڴ�ռ�
-*	unique_ptr<ָ���Ķ������ͣ�ɾ����>����ָ���������
-*	ɾ������������һ���ɵ��ö���
-*	shared_ptrɾ����ֱ���ڲ���������Ϻ������ּ��ɡ�����unique_ptr��ɾ������Ը���һ�㡣
-*	��Ҫ����ģ���д��ݾ������ͺ�ɾ���������͡�
+*(2)指定删除器
+*	1.缺省情况下，会用delete（默认删除器）释放内存空间
+*	unique_ptr<指定的对象类型，删除器>智能指针变量名。
+*	删除器本质上是一个可调用对象。（函数，重载了()的类对象，）
+*	shared_ptr删除器直接在参数里面加上函数名字即可。但是unique_ptr的删除器相对负载一点。
+*	需要向类模板中传递具体类型和删除函数类型。
 	unique_ptr<string, myf> p2(new string("jisuanj"), myDelete);
 *
-*	2.ָ��ɾ��������˵��
-*		shared_ptrָ����ɾ������ͬ������ָ��Ķ���������ͬ������shared_ptr����ͬһ�����͡�
-*		����unique_ptr�����Ͱ���ɾ������Ҳ��˵��ɾ������ͬ��unique_ptr����ͬһ�����͡�
+*	2.指定删除器额外说明
+*		shared_ptr指定的删除器不同，但是指向的对象类型相同，两个shared_ptr就是同一种类型。
+*		但是unique_ptr的类型包含删除器，也就说，删除器不同，unique_ptr不是同一个类型。
+*		s咱们在讲解shareed_ptr时，删除器不同，但是指向类型一样的shared_ptr可以放到同一个容器里，vector<share_ptr>，但是uniptr因为删除器不同，认为是不同的类型。。。
 *
-*(3)�ߴ�����
-*	shared_ptr����ָ���2��������unque_ptr����ָ����ͬ.����ɾ������ͬ����ռ���ڴ�ռ䲻ͬ
-*	lambda --4�ֽ�,
-*	����ָ������--8�ֽ�
-*	�����ֽڶ�Ч����Ӱ�죬�Զ���ɾ����Ӵ������ʹ�á�
-*	shared_ptr����ָ��ʲôɾ����������ָ���2����
+*(3)尺寸问题
+*	shared_ptr是裸指针的2倍，但是unque_ptr和裸指针相同.但是删除器不同可能占用内存空间不同
+*	d删除器的不同类型，导致unique_ptr的大小发生变化
+*	lambda --4字节,
+*	函数指针类型--8字节
+*	增加字节对效率有影响，自定义删除器哟啊谨慎使用。
+*	shared_ptr不管指定什么删除器都是裸指针的2倍。
 *
-*(4)����ָ���ܽ�
-*	4.1����ָ�뱳������˼��
-*		��ҪĿ�ģ����������ͷ��ڴ棬��ֹ�ڴ�й©��
-*	4.2auto_ptrΪʲô��������	--������⣬��Ĭ��ת������Ȩ
-*		c++98ʱ��������ָ�룬����unique_ptr����ָ���һ���ֹ��ܡ�
-*		�Ҳ����������б��棬�����ܴӺ����з��ء�
-*		��Ȼunique_ptr��auto_ptr���Ƕ�ռʽ�ģ�����auto_ptr���и�ֵ����=��Ĭ��ת������Ȩ��
-*	���飺ʹ��unique_ptrȡ��auto_ptr
+*(4)智能指针总结
+*	4.1智能指针背后的设计思想
+*		主要目的：帮助我们释放内存，防止内存泄漏。
+*	4.2auto_ptr为什么被废弃？	--设计问题，会默认转移所有权
+*		c++98时代的智能指针，具有unique_ptr智能指针的一部分功能。
+*		且不能在容器中保存，并不能从函数中返回。
+*		虽然unique_ptr和auto_ptr都是独占式的，但是auto_ptr进行赋值操作=会默认转移所有权，而unique_ptr编译时就会报错
+*		(
+*			auto_ptr<string> p1(nes string("sd");
+*			auto_ptr<string> p2=p1;//p1指向的对象转移给了p2了，p1已经为空了，这可以防止避免p1和p2析构一个对象两次
+*			y用p1(如果你没意识到p1已经空了)，代码就会崩溃。
+*			//unique_ptr<string> p2=p1;//报错。，避免后续继续使用p1导致崩溃的问题。
+*			d当然如果你用移动语义，也能达到auto_ptr的效果。
+*			{
+*				unique_ptr<string> p1(new string("sdsf"));
+*				unique_ptr<string> p2=std::move(p2);//ok，但是这样p1也会为空，也有空指针的风险。
+*
+*			}
+*		)
+*	建议：使用unique_ptr取代auto_ptr
 *	
-*	4.3����ָ���ѡ��
+*	4.3智能指针的选择
 *		shared_ptr
 *		unique_ptr
-*		��������ж��ָ��ָ��ͬһ������ʹ��shared_ptr
-*		�������Ҫ���ָ��ָ��ͬһ��ָ�룬ʹ��unique_ptrָ��
+*		如果程序有多个指针指向同一个对象，使用shared_ptr
+*		如果不需要多个指针指向同一个指针，使用unique_ptr指针
 *	
 *
 *
