c8fa057be1cfa922449393e06a3a848feb1ff525
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-03-30 22:04:44 +0800

    Update 001new和delete内部到底做了什么.cpp

--- 第7章内存高级话题/MemoryAdvanced/001new和delete内部到底做了什么.cpp
+++ 第7章内存高级话题/MemoryAdvanced/001new和delete内部到底做了什么.cpp
@@ -33,12 +33,12 @@ int main(void)
 	A *pa1 = new A;
 	A *pa2 = new A();
 
-	//2.4�������ͼӲ���()������
-	int *p3 = new int;//��ֵ���
-	int*p4 = new int();//��ֵ0
-	int*p5 = new int(100);//��ֵ100
+	//2.4基本类型加不加()的区别
+	int *p3 = new int;//初值随机
+	int*p4 = new int();//初值0
+	int*p5 = new int(100);//初值100
 
-	//2.5 new �����ʱ�򵽵�����ʲô
+	//2.5 new 对象的时候到底做了什么
 	A *p6 = new A();
 	delete p6;
 	/*
@@ -63,51 +63,52 @@ int main(void)
 
 	delete p6;
 	
-	008C54F5  mov         eax,dword ptr [p6]  
-	008C54F8  mov         dword ptr [ebp-164h],eax  
-	008C54FE  push        8  
-	008C5500  mov         ecx,dword ptr [ebp-164h]  
-	008C5506  push        ecx  
-	008C5507  call        operator delete (08C1050h)  
-	008C550C  add         esp,8  
-	008C550F  cmp         dword ptr [ebp-164h],0  
-	008C5516  jne         main+1E4h (08C5524h)  
-	008C5518  mov         dword ptr [ebp-16Ch],0  
-	008C5522  jmp         main+1F4h (08C5534h)  
-	008C5524  mov         dword ptr [p6],8123h  
-	008C552B  mov         edx,dword ptr [p6]  
-	008C552E  mov         dword ptr [ebp-16Ch],edx 
+	00916FC3  mov         ecx,dword ptr [this]  
+	00916FC6  call        `test23'::`2'::A::~A (09146B0h)  
+	00916FCB  mov         eax,dword ptr [ebp+8]  
+	00916FCE  and         eax,1  
+	00916FD1  je          `test23'::`2'::A::`scalar deleting destructor'+41h (0916FE1h)  
+	00916FD3  push        8  
+	00916FD5  mov         eax,dword ptr [this]  
+	00916FD8  push        eax  
+	00916FD9  call        operator delete (0904262h)  
+		call free()
+	00916FDE  add         esp,8  
+	00916FE1  mov         eax,dword ptr [this]  
+	00916FE4  pop         edi  
+	00916FE5  pop         esi  
 
-	�ܽ᣺������ operator new�������� operator new�����������malloc��Ȼ����ù��캯���������
-		delete��ʱ������ȵ�������������Ȼ�� operator delete�ͷ��ڴ����free()����
+	总结：调用了 operator new函数，在 operator new函数里面调用malloc。然后调用构造函数构造对象
+		delete的时候调用先调用析构函数，然后 operator delete释放内存调用free()函数
 	 */
 	system("pause");
 	return 0;
 }
 
 /*
-*(1)�����ͻع�
-*	������ģ�����е��ڴ��������Ҫ���á�
+*(1)综述和回顾
+*	对理解模板类中的内存分配有重要作用。
 *
-*(2)��new˵��
+*(2)从new说起
 *		A *pa1 = new A;
 		A *pa2 = new A();
-*	2.1new�����ʱ��Ӳ���()�Ĳ��
-*		����Ǹ����࣬����д��û������
-*		�����A���г�Ա����,��()��ʼ�����һЩ�ͳ�Ա�����йص��ڴ���0�����ǲ��������ڴ�ȫ����0��
-*	2.2	�����й��캯��������д��һ����
-*		��û�й��캯����������һ���麯����ʱ�򣬻��ǻ�ѳ�Ա����m_i��0.
-*		�ܽ᣺���û�й��캯����������������Ǹ�
+*	2.1new对象的时候加不加()的差别
+*		1)如果是个空类，两个写法没有区别，
+*		2)如果类A中有成员变量,带()初始化会把一些和成员变量有关的内存清0，但是不是整个内存全部清0；
+*		3)当类有构造函数，两种写法一样。（可能会初始化可能不会初始化为0，因为系统有时会认为是你的构造函数给接管了初始化的工作。）
+*		总结：如果没有构造函数，编译器会给我们给
 *
-*	2.3 ����()����һ�ֺ������õĸо�������()ʵ���ϻ��ǻ����Ĭ�Ϲ��캯����
-*	2.4�������ͼӲ���()������
-*	2.5 new �����ʱ�򵽵�����ʲô
-*		
-	�ܽ᣺������ operator new()�������� operator new�����������malloc��Ȼ����ù��캯���������
-		delete��ʱ������ȵ�������������Ȼ�� operator delete()�����ͷ��ڴ����free()����
-*(3)malloc����ʲô���飿malloc����ô�����ڴ�ģ�
-*	���ǲ���ϵͳ�ڲ����ڴ������ǣ�浽�ڴ���䣬�������ϲ����ͷŵ����⡣
-*	malloc����ò���ϵͳ���ײ�Ľ����ʵ���������ڴ���䡣
-*(4)�ܽ᣺
+*	2.3 加上()，有一种函数调用的感觉，不加()实际上还是会调用默认构造函数。
+*	2.4基本类型加不加()的区别
+		int *i1 = new int;//随机
+		int *i2 = new int();//0
+*	2.5 new 对象的时候到底做了什么
+*		new:关键字/操作符
+	总结：调用了 operator new()函数，在 operator new函数里面调用malloc。然后调用构造函数构造对象
+		delete的时候调用先调用析构函数，然后 operator delete()函数释放内存调用free()函数
+*(3)malloc做了什么事情？malloc是怎么分配内存的？
+*	这是操作系统内部的内存管理，牵涉到内存分配，管理，合并，释放等问题。
+*	malloc会调用操作系统更底层的借口来实现真正的内存分配。
+*(4)总结：
 *
 */
