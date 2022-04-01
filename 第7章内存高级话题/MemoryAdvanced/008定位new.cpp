51ee22e29bb7546195b0202bf6e3a35291f89025
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-04-01 13:25:28 +0800

    Update 008定位new.cpp

--- 第7章内存高级话题/MemoryAdvanced/008定位new.cpp
+++ 第7章内存高级话题/MemoryAdvanced/008定位new.cpp
@@ -22,21 +22,28 @@ class A
 public:
 	A()
 	{
-		cout << "������A���޲ι��캯��" << endl;
+		cout << "调用类A的无参构造函数" << endl;
 	}
 	A(int m):m_a(m)
 	{
-		cout << "�����в������캯��" << endl;
+		cout << "调用有参数构造函数" << endl;
 	}
 
 	~A()
 	{
-		cout << "������A����������" << endl;
+		cout << "调用类A的析构函数" << endl;
 	}
-	//��λnew����
+	
+	//传统new
 	void *operator new(size_t size,void*pHead)
 	{
-		return  pHead;//�յ�ԭʼ��ַ�����ؼ���
+		return  pHead;//收到原始地址，返回即可
+	}
+	
+	//定位new重载
+	void *operator new(size_t size,void*pHead)
+	{
+		return  pHead;//收到原始地址，返回即可
 	}
 
 public:
@@ -47,16 +54,23 @@ public:
 int main(void)
 {
 	//(1)
-	void *myPlaceNew = (void*)new char[sizeof(A)];//�ڴ�����ȷ������
-	A*myA = new (myPlaceNew)A();//�����޲ι��캯������myPlaceNew�������
+	void *myPlaceNew = (void*)new char[sizeof(A)];//内存必须先分配出来
+	// A*myA = A::A();// 一般构造函数不能手动调用，通过定位new来手动调用构造函数
+	A*myA = new (myPlaceNew)A();//调用无参构造函数，在myPlaceNew构造对象
+	//查看汇编：A*myA = new (myPlaceNew)A();
+		//call operator new(0811223H)//这里并没有调用malloc
+		//call _nmsp2::A::A(081144hH)//调用构造函数
+	//A *myA = new A();//调用传统new	
 	
 	void *myPlaceNew02 = (void*)new char[sizeof(A)];
-	A*myA02 = new (myPlaceNew02)A(2);//�����в������캯����myPlaceNew�������
-
-	//��ô����
-	myA->~A();//���캯���������ֶ����ã��������������ֶ�����
-	myA02->~A();
+	A*myA02 = new (myPlaceNew02)A(2);//调用有参数构造函数在myPlaceNew构造对象
+	
+	delete myA;//ok，也会去调用类对象的析构函数以及释放内存，等价于下面两句(下面的写法更加灵活)
+	//怎么析构
+	myA->~A();//构造函数不可以手动调用，析构函数可以手动调用
 	delete[](void*)myA;
+	
+	myA02->~A();
 	delete[](void*)myA02;
 
 
@@ -65,21 +79,21 @@ int main(void)
 }
 
 /*
-*(1)��λnew---placement new  ��ָ���Ŀռ乹�����
-*	1.��new������û��delete
-*	2.���ܣ����Ѿ������ԭʼ�ڴ��г�ʼ��һ������
-*		1.�Ѿ������ڴ棬��λnew���������ڴ棬����Ҫ�����ǽ������λnew����Ҫʹ�õ��ڴ�������
-*		2.��ʼ��һ������--��ʼ��һ��������ڴ�--���ö���Ĺ��캯����
-*	˵���ˣ�������һ������õ��ڴ�ռ��е��ù��캯������һ������
-*ʹ�ö�λnew�ﵽ���ù��캯����Ŀ��
+*(1)定位new(对比传统new)---placement new  在指定的空间构造对象
+*	1.有new，但是没有delete
+*	2.功能：在已经分配的原始内存中初始化一个对象。
+*		1.已经分配内存，定位new并不分配内存，你需要提起那将这个定位new所需要使用的内存分配出来
+*		2.初始化一个对象(初始化一个对象的内存)-->调用对象的构造函数。
+*	说白了，就是在一个分配好的内存空间中调用构造函数构造一个对象。
+*使用定位new达到调用构造函数的目的
 *
-*��ʽ��
-*new(��ַ) �����ͣ�������
+*格式：
+*new(地址) 类类型（参数）
 *
 *
-*�������غܶ�汾��operator new�����ǵ�һ�������ǹ̶��ģ�����size_t ��ʾ��Ҫnew�����sizeֵ��
+*可以重载很多版本的operator new，但是第一个参数是固定的，都是size_t 表示你要new对象的size值。
 *
-*2019��11��29��21:46:26
-* Sunrise�ڶ���������ѧ�ڶ���ѧ¥1121ʵ����
+*2019年11月29日21:46:26
+* Sunrise于东北电力大学第二教学楼1121实验室
 
 */
